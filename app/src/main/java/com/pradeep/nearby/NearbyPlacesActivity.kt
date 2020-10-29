6f8290af0cd8040c5f984df16038e831f68a2585
Pradeep Prasanth S <pradeepprasanths@gmail.com>
2020-10-29 20:50:13 +0530

    Integrating the nearby API page

--- app/src/main/java/com/pradeep/nearby/NearbyPlacesActivity.kt
+++ app/src/main/java/com/pradeep/nearby/NearbyPlacesActivity.kt
@@ -1,17 +1,26 @@
 package com.pradeep.nearby
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONException
 
 class NearbyPlacesActivity : AppCompatActivity() {
 
     private lateinit var lat: String
     private lateinit var lon: String
+    private lateinit var nearByPlacesList: ArrayList<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nearby_places)
+
+        val nearBy = findViewById<ListView>(R.id.nearbyPlaces)
 
         val extras = intent.extras
         extras?.let {
@@ -20,9 +29,34 @@ class NearbyPlacesActivity : AppCompatActivity() {
         }
 
         Log.i(TAG, "Latitude: $lat Longitude: $lon")
+
+        val requestQueue = Volley.newRequestQueue(this)
+
+        val url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query=&location=" +
+                "$lat,$lon&radius=10000&key=AIzaSyAg72yCWSbojIYhRq0-9G1cV5M66153yts"
+        val request =
+            JsonObjectRequest(Request.Method.GET, url, null, { response ->
+                try {
+                    val results = response.getJSONArray("results")
+                    for (i in 0 until results.length()) {
+                        val nearby = results.getJSONObject(i)
+                        val name = nearby.getString("name")
+                        nearByPlacesList.add(name)
+                        val adapter = ArrayAdapter(
+                            this,
+                            android.R.layout.simple_list_item_1,
+                            name.toList()
+                        )
+                        nearBy.adapter = adapter
+                    }
+                } catch (e: JSONException) {
+                    e.printStackTrace()
+                }
+            }, { error -> error.printStackTrace() })
+        requestQueue?.add(request)
     }
 
-    companion object{
+    companion object {
         private const val TAG = "NearbyPlacesActivity"
     }
 }
